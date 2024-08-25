1f6f887563b846a504956174cbed9d25effb52d6
virendra-pal <148178435+virendra-pal@users.noreply.github.com>
2024-08-25 04:47:04 +0000

    some basic concepts

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "virendrakumarp@gmailcom"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2
+
+accountEmail = "vkpal09@gmail.com"
+accountPassword = "56789"
+accountCity = "Bengaluru"
+
+
+console.log(accountId);
+
+console.table([accountEmail, accountId, accountPassword, accountCity])
